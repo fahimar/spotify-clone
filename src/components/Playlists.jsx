7824c0aa4f2f316360ab79d6fac2fd59ee1f2451
fahimar <fahimar14040@gmail.com>
2023-12-09 23:43:34 +0600

    Playlists work commited.

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import { useStateProvider } from "../utils/StateProvider";
 import axios from "axios";
+import reducer from "../utils/reducer";
+import { reducerCases } from "../utils/Constants";
+import styled from "styled-components";
 export default function Playlists() {
-  const [{ token, dispatch }] = useStateProvider();
+  const [{ token, playlists }, dispatch] = useStateProvider();
   useEffect(() => {
     const getPlaylistData = async () => {
       const response = await axios.get(
@@ -14,9 +17,65 @@ export default function Playlists() {
           },
         }
       );
-      console.log(response);
+      const { items } = response.data;
+      // console.log(response);
+      const playlists = items.map(({ name, id }) => {
+        return { name, id };
+      });
+      // console.log(playlists);
+      dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
     };
     getPlaylistData();
   }, [token, dispatch]);
-  return <div>Playlists</div>;
+  return (
+    <Container>
+      <ul>
+        {playlists.map(({ name, id }) => {
+          return <li key={id}>{name}</li>;
+        })}
+        {playlists.map(({ name, id }) => {
+          return <li key={id}>{name}</li>;
+        })}
+        {playlists.map(({ name, id }) => {
+          return <li key={id}>{name}</li>;
+        })}
+        {playlists.map(({ name, id }) => {
+          return <li key={id}>{name}</li>;
+        })}
+        {playlists.map(({ name, id }) => {
+          return <li key={id}>{name}</li>;
+        })}
+      </ul>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100%;
+  overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 52vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
+    li {
+      display: flex;
+      gap: 1 rem;
+      cursor: pointer;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+`;
